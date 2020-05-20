117afb859c917562578fcb9893e1543fec924bb7
Amilcar1998 <amilcar199819@gmail.com>
2020-05-20 11:46:37 -0600

    modificacion

--- models/Empleado.php
+++ models/Empleado.php
@@ -4,14 +4,153 @@
 class Empleado
 {
 private $idEmpleado;
+
+    /**
+     * @return mixed
+     */
+    public function getIdEmpleado()
+    {
+        return $this->idEmpleado;
+    }
+
+    /**
+     * @param mixed $idEmpleado
+     */
+    public function setIdEmpleado($idEmpleado)
+    {
+        $this->idEmpleado = $idEmpleado;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param mixed $nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getApellido()
+    {
+        return $this->apellido;
+    }
+
+    /**
+     * @param mixed $apellido
+     */
+    public function setApellido($apellido)
+    {
+        $this->apellido = $apellido;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCorreo()
+    {
+        return $this->correo;
+    }
+
+    /**
+     * @param mixed $correo
+     */
+    public function setCorreo($correo)
+    {
+        $this->correo = $correo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGenero()
+    {
+        return $this->genero;
+    }
+
+    /**
+     * @param mixed $genero
+     */
+    public function setGenero($genero)
+    {
+        $this->genero = $genero;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdRol()
+    {
+        return $this->idRol;
+    }
+
+    /**
+     * @param mixed $idRol
+     */
+    public function setIdRol($idRol)
+    {
+        $this->idRol = $idRol;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPass()
+    {
+        return $this->pass;
+    }
+
+    /**
+     * @param mixed $pass
+     */
+    public function setPass($pass)
+    {
+        $this->pass = $pass;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCargo()
+    {
+        return $this->cargo;
+    }
+
+    /**
+     * @param mixed $cargo
+     */
+    public function setCargo($cargo)
+    {
+        $this->cargo = $cargo;
+    }
 private $nombre;
 private $apellido;
 private $correo;
 private $genero;
 private $idRol;
 private $pass;
-private $idRol;
+private $cargo;
 
+    public function __construct($idEmpleado, $nombre, $apellido, $correo, $genero, $idRol, $pass, $cargo)
+    {
+        $this->idEmpleado = $idEmpleado;
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+        $this->correo = $correo;
+        $this->genero = $genero;
+        $this->idRol = $idRol;
+        $this->pass = $pass;
+        $this->cargo = $cargo;
+    }
 
 
 }
