f48fb5981ccd42c599d5874efd7b47e62e35dca5
Amilcar1998 <amilcar199819@gmail.com>
2020-05-20 19:58:21 -0600

    actualiacion

--- controllers/controllerEmpleado.php
+++ controllers/controllerEmpleado.php
@@ -5,11 +5,9 @@ $obEmp=new EmpleadoModel();
 
 if(isset($_REQUEST["insertar"])){
 
- $e= new Empleado($_REQUEST['txtIdEmpleado'],$_REQUEST['txtNombreE'],$_REQUEST['txtApellidos'],$_REQUEST['txtCorreo'],$_REQUEST['txtGenero'],$_REQUEST['idRol'],sha1($_REQUEST['txtPass']),$_REQUEST['txtCargo'],);
-
+ $e= new Empleado($_REQUEST['txtIdEmpleado'],$_REQUEST['txtNombreE'],$_REQUEST['txtApellidos'],$_REQUEST['txtCorreo'],$_REQUEST['txtGenero'],$_REQUEST['idRol'],sha1($_REQUEST['txtPass']),$_REQUEST['txtCargo']);
       $obEmp->insertarEmpleado($e);
-      echo "<script>alert('buen trabajo');</script>";
-      echo "<script>Swal.fire('Good job!','You clicked the button!','success');</script>";
+
 }
   $datos=$obEmp->getEmpleado();
   $data=$obEmp->getRol();
