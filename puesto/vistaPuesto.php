a8d06aec3000c7208b05d7abda08adac83edc39a
Amilcar1998 <amilcar199819@gmail.com>
2020-06-04 17:23:43 -0600

    actualizacion

--- puesto/vistaPuesto.php
+++ puesto/vistaPuesto.php
@@ -19,7 +19,7 @@ $dao= new DAOPuesto();
   <title>SB Admin - Tables</title>
 
   <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="../controllers/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 
   <!-- Page level plugin CSS-->
   <link href="../controllers/vendor/datatables/dataTables.bootstrap4.css" rel="stylesheet">
